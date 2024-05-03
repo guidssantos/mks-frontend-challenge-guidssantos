207cca56e6a2bc12de83f566dd3c6500e969b986
guidssantos <guilhermedssantos1@gmail.com>
2024-05-03 10:01:36 -0300

    feat: styles global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,8 +4,41 @@ const GlobalStyles = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap');
 body {
   font-family: 'Montserrat', sans-serif;
+
 }
 * {
+
+.Toastify__toast-container {
+      z-index: 9999999999999;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+.Toastify__toast-body{
+  gap: 1rem;
+}
+    .Toastify__toast-body {
+      > div:last-of-type {
+        font-size: 1.4rem;
+      }
+    }
+
+  ::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #007bff;
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #1e90ff;
+}
     margin: 0;
     padding: 0;
     box-sizing: border-box;
