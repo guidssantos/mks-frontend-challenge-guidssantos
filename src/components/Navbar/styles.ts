1209a28fcb94cffe3991d84e42ae5d1c9021f681
guidssantos <guilhermedssantos1@gmail.com>
2024-05-03 09:59:56 -0300

    feat: navbar implementation

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -6,7 +6,11 @@ background-color: ${theme.colors.primary};
 display: flex;
 justify-content: space-between;
 align-items: center;
-padding: 2rem 5rem
+padding: 2rem 5rem;
+
+@media (max-width: 425px){
+  padding: 2rem 2rem;
+}
 `}
 `
 
@@ -21,6 +25,10 @@ export const LogoTitle = styled.h1`
     font-size: 4rem;
     color: ${theme.colors.white};
     font-weight: 600;
+
+    @media (max-width: 520px){
+      font-size: 3rem;
+    }
   `}
 `
 
@@ -29,5 +37,8 @@ export const LogoSubTitle = styled.h2`
     font-size: 2rem;
     color: ${theme.colors.white};
     font-weight: 300;
+    @media (max-width: 520px) {
+      font-size: 1.6rem;
+    }
   `}
 `
