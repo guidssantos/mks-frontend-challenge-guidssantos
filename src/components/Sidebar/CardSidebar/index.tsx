51785055eb836ab3e59b85de9d850c35c8857531
guidssantos <guilhermedssantos1@gmail.com>
2024-05-03 10:00:41 -0300

    feat: CardSidebar with styles and effect and mobile

--- src/components/Sidebar/CardSidebar/index.tsx
+++ src/components/Sidebar/CardSidebar/index.tsx
@@ -1,23 +1,67 @@
-'use client'
+import  React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import { useDispatch } from 'react-redux'
 import * as Styled from './styles'
-import AppleWatch from '../../Card/IndividualCard/images/apple-watch.svg'
+import { toast } from 'react-toastify'
+import { removeFromCart, updateAmount } from '@/store/modules/cart/actions'
+import { ProductProps } from '@/types/products'
 
+interface CardSidebarProps {
+  item: ProductProps
+}
 
-export const CardSidebar = () => {
+export const CardSidebar = ({ item }: CardSidebarProps) => {
+  const dispatch = useDispatch()
+  const [quantity, setQuantity] = useState<number>(item.amount ? item.amount : 0)
+
+  useEffect(() => {
+    if (item.amount) setQuantity(item.amount)
+  }, [item.amount])
+
+  const handleRemoveFromCart = () => {
+    toast.success('Produto removido com sucesso!')
+    dispatch(removeFromCart(item.id))
+  }
+
+  const handleUpdateAmount = (newAmount: number) => {
+    if (newAmount > 1) {
+      dispatch(updateAmount({ id: item.id, amount: newAmount }))
+      setQuantity(newAmount)
+    } else {
+      return
+    }
+  }
+
+  const totalPrice = item.price * quantity
+
   return (
     <Styled.Container>
-      <Styled.OnClose>
-        X
-      </Styled.OnClose>
-      <Image src={AppleWatch} alt="Apple watch" width={46} height={46} />
-      <Styled.Title>Apple Watch Series 4 GPS</Styled.Title>
-      <Styled.Quantity>
-        0
-      </Styled.Quantity>
-      <Styled.Price>
-        R$:399
-      </Styled.Price>
+      <Styled.OnClose onClick={handleRemoveFromCart}>X</Styled.OnClose>
+      <Image src={item.photo} alt={item.name} width={46} height={46} />
+      <Styled.Title>{item.name}</Styled.Title>
+      <Styled.QuantityContainer>
+        <Styled.QuantityTitle>Qtd:</Styled.QuantityTitle>
+        <Styled.Quantity>
+          <Styled.QuantityControl
+            style={{
+              borderRight: '0.2px solid #bfbfbf'
+            }}
+            onClick={() => handleUpdateAmount(quantity - 1)}
+          >
+            -
+          </Styled.QuantityControl>
+          <span>{quantity}</span>
+          <Styled.QuantityControl
+            style={{
+              borderLeft: '0.2px solid #bfbfbf'
+            }}
+            onClick={() => handleUpdateAmount(quantity + 1)}
+          >
+            +
+          </Styled.QuantityControl>
+        </Styled.Quantity>
+      </Styled.QuantityContainer>
+      <Styled.Price>R$: {totalPrice.toFixed(2)}</Styled.Price>{' '}
     </Styled.Container>
   )
 }
--- src/components/Sidebar/CardSidebar/styles.ts
+++ src/components/Sidebar/CardSidebar/styles.ts
@@ -22,15 +22,43 @@ align-items: center;
 justify-content: center;
 width: 20px;
 border-radius: 1rem;
+cursor: pointer;
 `
 
 
 export const Title = styled.h1`
   font-weight: 400;
   font-size: 1.3rem;
+  width: 100px;
 `
 
-export const Quantity = styled.div``
+export const QuantityContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap: 0.6rem;
+`
+
+export const QuantityTitle = styled.h1`
+  font-weight: 400;
+ font-size : 0.8rem;
+ margin: 0;
+ padding: 0;
+`
+
+export const Quantity = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+  border: 0.3px solid #bfbfbf;
+  border-radius: 4px;
+`
+
+export const QuantityControl = styled.div`
+  padding: 0 0.8rem;
+  margin: 0.6rem 1px;
+  color: black;
+  cursor: pointer;
+`
 
 export const Price = styled.span`
 font-size: 1.4rem;
