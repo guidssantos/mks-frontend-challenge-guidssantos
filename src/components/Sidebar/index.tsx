d8b854d2e8b4068b9e988fb5541bdc6b7c4878d2
guidssantos <guilhermedssantos1@gmail.com>
2024-05-03 10:00:16 -0300

    feat: Sidebar implementation with SidebarCard

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,22 +1,68 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { CardSidebar } from './CardSidebar'
 import * as Styled from './styles'
+import { visibleCard, clearCart } from '@/store/modules/cart/actions'
+import { motion } from 'framer-motion'
+import { toast } from 'react-toastify'
+import { ProductProps } from '@/types/products'
 
-export const Sidebar = ({ visible  }: { visible?: boolean }) =>
-  visible ? (
-    <Styled.Container>
-      <Styled.ItemContainer>
-        <Styled.Header>
-          <Styled.Title>Carrinho de compras</Styled.Title>
-          <Styled.OnClose>X</Styled.OnClose>
-        </Styled.Header>
-        <Styled.ContentContainer>
-          <CardSidebar />
-          <Styled.TotalContainer>
-            <Styled.TotalTitle>Total:</Styled.TotalTitle>
-            <Styled.TotalPrice>R$: 798</Styled.TotalPrice>
-          </Styled.TotalContainer>
-        </Styled.ContentContainer>
-      </Styled.ItemContainer>
-      <Styled.Button>Finalizar Compra</Styled.Button>
-    </Styled.Container>
-  ) : null
+export const Sidebar = ({ visible }: { visible?: boolean }) => {
+  const dispatch = useDispatch()
+  const cartItems = useSelector((state: any) => state.cart.items)
+
+  const handleShoppingCartClick = () => {
+    if (visible) {
+      dispatch(visibleCard(false))
+    }
+  }
+
+  const handleFinalizarCompraClick = () => {
+    toast.success('Compra realizada com sucesso!')
+    dispatch(visibleCard(false))
+    dispatch(clearCart())
+  }
+
+  const totalPrice = cartItems.reduce(
+    (total: number, item: {price: number; amount: number}) => total + item.price * item.amount,
+    0
+  )
+
+  return (
+    <motion.div
+      animate={{
+        x: visible ? 0 : '-1%',
+        opacity: visible ? 1 : 0
+      }}
+      transition={{ type: 'tween', duration: 0.3 }}
+      style={{
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+        visibility: visible ? 'visible' : 'hidden'
+      }}
+    >
+      <Styled.Container>
+        <Styled.ItemContainer>
+          <Styled.Header>
+            <Styled.Title>Carrinho de compras</Styled.Title>
+            <Styled.OnClose onClick={handleShoppingCartClick}>X</Styled.OnClose>
+          </Styled.Header>
+          <Styled.ContentContainer>
+            <Styled.CardContainer>
+              {cartItems.map((item: ProductProps) => (
+                <CardSidebar key={item.id} item={item} />
+              ))}
+            </Styled.CardContainer>
+            <Styled.TotalContainer>
+              <Styled.TotalTitle>Total:</Styled.TotalTitle>
+              <Styled.TotalPrice>R$: {totalPrice.toFixed(2)}</Styled.TotalPrice>
+            </Styled.TotalContainer>
+          </Styled.ContentContainer>
+        </Styled.ItemContainer>
+        <Styled.Button onClick={handleFinalizarCompraClick}>
+          Finalizar Compra
+        </Styled.Button>{' '}
+      </Styled.Container>
+    </motion.div>
+  )
+}
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,18 +1,41 @@
 import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
-${({theme}) => css`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: ${theme.colors.primary};
+    height: 100vh;
+    box-shadow: ${theme.shadows.default};
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 33%;
+    transition: transform 0.3s ease;
+
+    &.show {
+      right:0;
+    }
+
+    &.hide {
+      right:-100%;
+    }
+
+    @media (max-width: 425px){
+      width: auto;
+      position: fixed;
+    }
+  `}
+`
+
+export const CardContainer = styled.div`
 display: flex;
 flex-direction: column;
-justify-content: space-between;
-background-color: ${theme.colors.primary};
-height: 100%;
-box-shadow: ${theme.shadows.default};
-position: absolute;
-right: 0;
-top: 0;
-width: 33%;
-`}
+gap: 1.2rem;
+overflow-y: scroll;
+overflow-x: hidden;
+padding: 1.2rem;
 `
 
 export const ItemContainer = styled.div`
@@ -34,7 +57,18 @@ font-weight: 700;
 width: 200px;
 `
 
-export const OnClose = styled.div``
+export const OnClose = styled.div`
+  background-color: black;
+  color: white;
+  height: 30px;
+  width: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 2rem;
+  font-size: 1.4rem;
+  cursor: pointer;
+`
 
 export const Button = styled.button`
   color: white;
