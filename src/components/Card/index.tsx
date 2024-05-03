846ce75bd8e2264c2c3549e703d268874cf2062a
guidssantos <guilhermedssantos1@gmail.com>
2024-05-03 09:59:08 -0300

    feat: add card, loading and initial IndividualCards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,63 @@
-'use client'
+import useProducts from '@/hooks/useProducts'
 import { IndividualCard } from './IndividualCard'
 import * as Styled from './styles'
+import { motion, useCycle } from 'framer-motion'
+import { ProductProps } from '@/types/products'
 
 export const Card = () => {
+  const { data, isLoading, isError } = useProducts({
+    page: 1,
+    rows: 8,
+    sortBy: 'name',
+    orderBy: 'DESC'
+  })
+
+  const [opacityCycle, cycleOpacity] = useCycle(1, 0.5)
+
+  if(isError){
+    console.log('error', isError)
+  }
+
+  if (isLoading) {
+    return (
+      <Styled.Container>
+        {[...Array(8)].map((_, index) => (
+          <motion.div
+            key={index}
+            animate={{ opacity: opacityCycle }}
+            transition={{ duration: 1.5, repeat: Infinity }}
+            style={{ marginBottom: '20px', width: '217.56px', height: '316px' }}
+            onAnimationComplete={() => cycleOpacity()}
+          >
+            <div
+              style={{
+                width: '100%',
+                height: '100%',
+                backgroundColor: '#f0f0f0',
+                borderRadius: '8px'
+              }}
+            ></div>
+          </motion.div>
+        ))}
+      </Styled.Container>
+    )
+  }
+
   return (
     <Styled.Container>
-      <IndividualCard/>
+      {data.products.map((product: ProductProps) => (
+        <motion.div
+          key={product.id}
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <IndividualCard
+            key={product.id}
+            product={product}
+          />
+        </motion.div>
+      ))}
     </Styled.Container>
   )
 }
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,8 +1,19 @@
 import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  margin: 0 auto;
+  gap: 2.6rem;
+  @media (max-width: 980px) {
+    grid-template-columns: repeat(3, 1fr);
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 425px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
