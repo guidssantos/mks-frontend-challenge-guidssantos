d0fbabbcb6061541778303a59abe43893eae2717
guidssantos <guilhermedssantos1@gmail.com>
2024-05-12 09:49:37 -0300

    fix: new style in IndividualCard

--- src/components/Card/IndividualCard/index.tsx
+++ src/components/Card/IndividualCard/index.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import * as S from './styles'
 import AppleWatch from './images/apple-watch.svg'
 import ShoppingBag from './images/shopping-bag.svg'
-import { useDispatch } from 'react-redux'
-import { addToCart } from '@/store/modules/cart/actions'
+import HeartIcon from './images/heart.svg'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart, favorite } from '@/store/modules/cart/actions'
 import { toast } from 'react-toastify'
 import { ProductProps } from '@/types/products'
+import { formatCurrency } from '@/utils/formatCurrency'
 
 
 interface IndividualCardProps {
@@ -15,33 +17,54 @@ interface IndividualCardProps {
 
 export const IndividualCard = ({ product }: IndividualCardProps) => {
   const dispatch = useDispatch()
+  const favorites = useSelector((state: any) => state.cart.favorites)
+
+  const isProductInFavorites = favorites.some(
+    (favorite: any) => favorite._id === product._id
+  )
 
   const handleAddToCart = () => {
     toast.success('Produto adicionado ao carrinho!')
     dispatch(addToCart(product))
   }
+
+  const handleFavorite = () => {
+    dispatch(favorite(product))
+  }
+
+
   return (
     <S.Container>
       <S.CardDetailsContainer>
+        <S.Header>
+          <S.HeaderBestPrice>MELHOR PREÇO</S.HeaderBestPrice>
+          <S.Favorite onClick={handleFavorite} selected={isProductInFavorites}>
+            <HeartIcon />
+          </S.Favorite>
+        </S.Header>
         <Image
           src={product?.photo}
           alt={product?.name}
-          width={150}
-          height={150}
+          width={188}
+          height={188}
+          style={{ objectFit: 'contain' }}
         />
         <S.TitleContainer>
           <S.Title>{product?.name}</S.Title>
-          <S.PriceContainer>
-            <S.Price>R${product?.price}</S.Price>
-          </S.PriceContainer>
         </S.TitleContainer>
-        <S.Description>{product?.description}</S.Description>
+        <S.PriceContainer>
+          <S.PriceTitle>À vista no PIX</S.PriceTitle>
+          <S.Price>{formatCurrency(product?.price * 0.85)}</S.Price>
+        </S.PriceContainer>
+        <S.PriceContainer>
+          <S.PriceTitle>A prazo</S.PriceTitle>
+          <S.SubPrice>{formatCurrency(product?.price)}</S.SubPrice>
+        </S.PriceContainer>
+        <S.Button onClick={handleAddToCart}>
+          Comprar
+          <ShoppingBag />
+        </S.Button>
       </S.CardDetailsContainer>
-
-      <S.Button onClick={handleAddToCart}>
-        <Image src={ShoppingBag} alt="Shopping Bag" />
-        COMPRAR
-      </S.Button>
     </S.Container>
   )
 }
--- src/components/Card/IndividualCard/styles.ts
+++ src/components/Card/IndividualCard/styles.ts
@@ -4,13 +4,13 @@ export const Container = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
-    align-items: center;
-    box-shadow: ${theme.shadows.default};
-    width: 217.56px;
-    gap: 1rem;
+    width: 263.5px;
     border-radius: ${theme.border.radius.xsmall};
+    background-color: ${theme.colors.white};
     justify-content: space-between;
     height: 100%;
+    border: 1px solid #f0f0f0;
+    padding: 1.4rem;
   `}
 `
 
@@ -19,9 +19,9 @@ export const CardDetailsContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 1rem 1rem 0 1rem;
-    gap: 1rem;
-
+    gap: 0.7rem;
+    height: 100%;
+    justify-content: space-around;
   `}
 `
 export const TitleContainer = styled.div`
@@ -30,24 +30,48 @@ gap: 1.6rem;
 `
 
 export const Title = styled.h1`
-  line-break: auto;
-  font-weight: 400;
-  font-size: 1.6rem;
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 120%;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0em;
+  font-variation-settings: 'opsz' auto;
+  color: #3d3d3d;
 `
 
 export const PriceContainer = styled.div`
   ${({ theme }) => css`
-    height: fit-content;
-    background-color: ${theme.colors.secondary};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
     padding: 0.3rem 0.6rem;
     border-radius: ${theme.border.radius.xsmall};
   `}
 `
 
 export const Price = styled.h2`
-  font-weight: 700;
-  font-size: 1.5rem;
-  color: white;
+  font-size: 17.6px;
+  font-weight: bold;
+  line-height: 120%;
+  text-align: right;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0em;
+  color: #4d4dff;
+`
+
+export const SubPrice = styled.h2`
+  font-size: 14.08px;
+  font-weight: 500;
+  line-height: 120%;
+  text-align: right;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0em;
+  font-variation-settings: 'opsz' auto;
+  color: #4d4dff;
 `
 
 export const Description = styled.span`
@@ -61,13 +85,13 @@ export const Button = styled.button`
     display: flex;
     align-items: center;
     gap: 1.6rem;
+    width: 100%;
     justify-content: center;
     background-color: ${theme.colors.primary};
-    width: 100%;
     padding: 1rem;
     color: ${theme.colors.white};
     font-weight: 600;
-    border-radius: 0 0 ${theme.border.radius.xsmall}
+    border-radius:
       ${theme.border.radius.xsmall};
     transition: background-color 0.3s ease;
     &:hover {
@@ -80,6 +104,58 @@ export const Button = styled.button`
         width: 13px;
         height: 100%;
       }
+    }
+
+    path{
+      fill: #fff;
+    }
+  `}
+`
+export const PriceTitle = styled.h2`
+  font-size: 14px;
+  font-weight: 300;
+  line-height: 120%;
+  text-align: right;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0em;
+  font-variation-settings: 'opsz' auto;
+  color: #4d4dff;
+`
+export const Header = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+width: 100%;
+`
+
+export const HeaderBestPrice = styled.h2`
+  border-radius: 7.04px;
+  opacity: 1;
+  background: #09066a;
+  padding: 7.04px;
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 120%;
+  text-align: right;
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+  letter-spacing: 0.01em;
+  font-variation-settings: 'opsz' auto;
+  color: #ffffff;
+`
+
+export const Favorite = styled.div<any>`
+  ${({ selected }) => css`
+    display: flex;
+    border-radius: 8.96px;
+    opacity: 1;
+    background: #a6bcff;
+    padding: 0.6rem;
+    cursor: pointer;
+    path {
+      fill: ${selected ? '#3D3DC9' : '#fff'};
     }
   `}
 `
